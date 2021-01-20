1db2233d7b6af8e42b1bfe5e569a3e422f403e90
Filipe <flpfilipe19@gmail.com>
2021-01-20 18:09:31 -0300

    envio email node

--- app.js
+++ app.js
@@ -3,30 +3,37 @@ const express = require('express')
 
 const app = express()
 
-var remetente = nodemailer.createTransport({
-    host: 'mail.estefanemedeiros.com',
-    service: 'mail.estefanemedeiros.com',
-    port: 587,
-    secure: true,
-    auth: {
-        user: 'contato@estefanemedeiros.com.br',
-        pass: '@Estefane1'
-    }
-});
+app.get('/', () => {
 
-var emailASerEnviado = {
-    from: 'gnr_filipe@hotmail.com',
-    to: 'contato@estefanemedeiros.com.br',
-    subject: 'Enviando Email com Node.js',
-    text: 'Estou te enviando este email com node.js',
-};
+    var remetente = nodemailer.createTransport({
+        host: 'smtp.live.com',
+        secureConnection: false,
+        port: 587,
+        auth: {
+            user: 'gnr_filipe@hotmail.com',
+            pass: 'flp190295'
+        },
+        tls: {
+            ciphers: 'SSLv3'
+        }
+    });
 
-remetente.sendMail(emailASerEnviado, function (error) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email enviado com sucesso.');
-    }
-});
+    var emailASerEnviado = {
+        from: 'gnr_filipe@hotmail.com',
+        to: ['contato@estefanemedeiros.com', 'gnr_filipe@hotmail.com'],
+        replyTo: 'contato@teste.com',
+        subject: 'Enviando Email com Node.js',
+        text: 'Estou te enviando este email com node.js',
+    };
+
+    remetente.sendMail(emailASerEnviado, function (error, a, b) {
+        console.log(error)
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email enviado com sucesso.');
+        }
+    });
+})
 
 app.listen(3001)
